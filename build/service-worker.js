bbc671b1f534529cfce4938df2d7a19cfc149baa
Jordy Alkema <me@jordyalkema.nl>
2021-05-19 01:46:02 +0200

    Added 'Cheap Reflect'

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.51169954bfd9b03ebcdc3dd2dc964cd5.js"
+  "./precache-manifest.2406a9b41e456c739993dbb5e5edf6f2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
